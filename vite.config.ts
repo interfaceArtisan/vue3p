db4421ba8fb59b219e1d3b964b15c4285f4cbe83
pengxing <979698753@qq.com>
2024-10-14 16:10:11 +0800

    修改vite base配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue3p/',
   build: {
-    outDir: 'docs',
+    outDir: 'dist', // 默认为dist，可不配置
   },
   server: {
     proxy: {
